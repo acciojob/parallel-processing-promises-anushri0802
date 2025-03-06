d61012290ecb6f510d0957017989f413d2ba8e34
Anushri <112797477+anushri0802@users.noreply.github.com>
2025-03-06 12:33:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(url) {
+    return new Promise ((resolve, reject) => {
+        const img = new Image();
+        img.src = url;
+        img.onload = () => resolve(img);
+        img.onerror = () => reject(new Error("Image download failed"));
+    });
+}
+
+function downloadImages() {
+    output.innerHTML = "<p>Loading images, please wait...</p>";
+
+    const promises = images.map(imgObj => downloadImage(imgObj.url));
+    Promise.all(promises)
+        .then((images) => {
+			output.innerHTML = "";
+            images.forEach((img) => output.appendChild(img));
+        })
+        .catch((err) => {
+            error.innerText = err.message;
+        })
+}
+
+btn.addEventListener("click", downloadImages);
